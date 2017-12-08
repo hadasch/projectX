e1ab91ded1a27039072460e01bb74b28d9beaa20
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2017-12-08 14:49:43 +0100

    Two Changes
    - working directory is not needed
    - VC extraction in FA-Modell works now

--- examplecode/FA-Modell Eberhart-Russell.R
+++ examplecode/FA-Modell Eberhart-Russell.R
@@ -1,6 +1,5 @@
 require(asreml)
-wd <- paste("D:/User/pschmidt/bwSyncAndShare/Yield Stability/examplecode")
-setwd(wd)
+
 GxEmeans <- read.delim("GxE_means.txt")
 
 ########################################
@@ -10,11 +9,19 @@ GxEmeans <- read.delim("GxE_means.txt")
 ER_fa_mod <- asreml(fixed   = GxE_mean ~ G,
                     random  = ~ fa(G,k=1):E,
                     family  = asreml.gaussian(dispersion=10e-5),
-                    control = asreml.control(pworkspace=8e8 ,
+                    control = asreml.control(pworkspace=8e8,
                                              workspace=8e8, 
                                              maxiter=100),
                     data    = GxEmeans)
 
-ER_fa_mod_vc  <- summary(ER_fa_mod)$varcomp[2:3]
-ER_fa_mod_vc$temp <- rownames(ER_fa_mod_vc)[2:dim(ER_fa_mod_vc)[1]]
-ER_fa_mod_vc$test <- gregexpr("G.",ER_fa_mod_vc$temp)
+# format variance component output
+VC  <- summary(ER_fa_mod)$varcomp[2:3]
+VC$name <- gsub("E!G.","",substr(rownames(VC),regexpr("!",rownames(VC))-1,nchar(rownames(VC))))
+row.names(VC) <- NULL
+
+VC_t <- data.frame(G     = unique(substr(VC$name,1,nchar(VC$name)-4)[1:(dim(VC)[1]-1)/2]),
+                   Var   = VC[which(substr(VC$name,nchar(VC$name)-2,nchar(VC$name))=="var"),"component"],
+                   Lamda = VC[which(substr(VC$name,nchar(VC$name)-2,nchar(VC$name))=="fa1"),"component"])
+
+
+

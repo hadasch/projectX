2454857cc53fb8d33b8c6294de3f22900c6fa929
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2017-12-11 11:40:27 +0100

    Tippfehler SED_yield

--- CODE/01 data import.R
+++ CODE/01 data import.R
@@ -7,7 +7,7 @@ setwd(wd)     #change back to original wd
 Aman$x <- "Aman"
 Boro$x <- "Boto"
 dat <- rbind(Aman,Boro)
-colnames(dat) <- c("GL_v","GY_v","GLY_v","E_v","L","Y","rep","yield","x")
+colnames(dat) <- c("GL_v","GY_v","GLY_v","E_v","L","Y","rep","sed_yield","x")
 
 head(dat) 
 # Correct dataset? In SAS there is "Year_of_release" in first data step

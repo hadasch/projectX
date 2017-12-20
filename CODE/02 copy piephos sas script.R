b2f17cee523b65ec166a2136125b64770b72704c
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2017-12-20 12:22:28 +0100

    R-Code Änderung von "AMAN" zu "DAT"
    um es allgemeingültiger zu machen

--- CODE/02 copy piephos sas script.R
+++ CODE/02 copy piephos sas script.R
@@ -1,32 +1,32 @@
 rm(list = ls())
-aman <- read.delim("aman.txt")
-aman$Y <- as.factor(aman$Y)
-aman$Rep <- as.factor(aman$Rep)
-aman$GG <- as.factor(paste(aman$Group,aman$G,sep="-"))
+dat <- read.delim("aman.txt")
+dat$Y   <- as.factor(dat$Y)
+dat$Rep <- as.factor(dat$Rep)
+dat$GG  <- as.factor(paste(dat$Group,dat$G,sep="-"))
 
 # arithmetic means
-arimean <- tapply(aman$Yield,paste(aman$Env,aman$GG),mean,na.rm=F)
-aman2   <- data.frame(BY=rownames(arimean),arimean=arimean)
-aman2$Env <- as.factor(substr(aman2$BY,1,regexpr(" ",aman2$BY)-1))
-aman2$GG  <- as.factor(substr(aman2$BY,  regexpr(" ",aman2$BY)+1,nchar(as.character(aman2$BY))))
-rownames(aman2) <- NULL
-aman2 <- aman2[,c("Env","GG","arimean")]
-attr(aman2$arimean,"dimnames") <- NULL 
+arimean  <- tapply(dat$Yield,paste(dat$Env,dat$GG),mean,na.rm=F)
+dat2     <- data.frame(BY=rownames(arimean),arimean=arimean)
+dat2$Env <- as.factor(substr(dat2$BY,1,regexpr(" ",dat2$BY)-1))
+dat2$GG  <- as.factor(substr(dat2$BY,  regexpr(" ",dat2$BY)+1,nchar(as.character(dat2$BY))))
+rownames(dat2) <- NULL
+dat2 <- dat2[,c("Env","GG","arimean")]
+attr(dat2$arimean,"dimnames") <- NULL 
 
 # lsmeans (stage I)
-lsmeans <- matrix(list(),length(unique(aman$Env)),1)
+lsmeans <- matrix(list(),length(unique(dat$Env)),1)
 
 require(asreml)
-for (i in c(1:length(unique(aman$Env)))){
+for (i in c(1:length(unique(dat$Env)))){
   mod <- asreml(fixed = Yield ~ Rep + GG,
-                data  = subset(aman, Env==aman$Env[i]))
+                data  = subset(dat, Env==dat$Env[i]))
   
   pred <- predict(mod,classify="GG")$predictions$pvals[,c(1:3)]
   names(pred) <- c("GG","adjmean","se")
   pred        <- subset(pred,is.na(adjmean)==F)
   pred$ErrVar <- pred$se*pred$se
   pred$w      <- 1/pred$ErrVar
-  pred$Env    <- as.factor(paste(unique(aman$Env[i])))
+  pred$Env    <- as.factor(paste(unique(dat$Env[i])))
   pred        <- pred[,c("Env","GG","adjmean","se","ErrVar","w")]
   
   lsmeans[[i,1]] <- pred
@@ -34,10 +34,13 @@ for (i in c(1:length(unique(aman$Env)))){
 
 # Combine into Mean-Table
 require(plyr)
-aman3 <- ldply(lsmeans, data.frame)
-aman3 <- merge(aman2, aman3, by=c("Env","GG"))
-aman3 <- merge(unique(aman[,!names(aman) %in% c("Rep","Yield")]),
-               aman3, by=c("Env","GG"))
+dat3 <- ldply(lsmeans, data.frame)
+dat3 <- merge(dat2, dat3, by=c("Env","GG"))
+dat3 <- merge(unique(dat[,!names(dat) %in% c("Rep","Yield")]),
+               dat3, by=c("Env","GG"))
+
+# Clean up
+rm(list=ls()[ls() %in% c("pred","i","arimean","mod","lsmeans")])
 
 # ...
 # Skipped some SAS code
@@ -45,7 +48,7 @@ aman3 <- merge(unique(aman[,!names(aman) %in% c("Rep","Yield")]),
 
 # Shukla's Stability Variance
 #############################
-aman3 <- aman3[order(aman3$G,aman3$Env),] 
+dat3 <- dat3[order(dat3$G,dat3$Env),] 
 
 shuklamod <- asreml(fixed   = adjmean ~ xj + tj,
                     random  = ~ G + L + Y + L:G + Y:G + Y:L + at(G):Env, 
@@ -55,7 +58,7 @@ shuklamod <- asreml(fixed   = adjmean ~ xj + tj,
                     control = asreml.control(pworkspace=8e10 ,
                                              workspace=8e8, 
                                              maxiter=100),
-                    data    = aman3, ran.order = "user")
+                    data    = dat3, ran.order = "user")
 
 # Format VC estimates
 shukla_vc  <- summary(shuklamod)$varcomp[2:3]
@@ -68,17 +71,17 @@ shukla_vc$yesno <- "no"
 shukla_vc[which(regexpr(":Env",shukla_vc$VC)>0),"yesno"] <- "yes"
 shukla_vc$StabVar <- NA
 shukla_vc[which(shukla_vc$yesno=="yes"),"StabVar"] <- gsub(" :Env","",shukla_vc[which(shukla_vc$yesno=="yes"),"VC"])                          
-shukla_vc$StabVar <- as.factor(shukla_vc$StabVar)
+shukla_vc$StabVar   <- as.factor(shukla_vc$StabVar)
 rownames(shukla_vc) <- NULL
 shukla_vc <- shukla_vc[,c(3,1,2,5)]
 colnames(shukla_vc) <- c("VC","Estimate","StdErr","V")
 
-shuklaVs <- subset(shukla_vc, is.na(shukla_vc$V)==F)
+shukla_out <- subset(shukla_vc, is.na(shukla_vc$V)==F)
 
 # Display Results 
 require(ggplot2)
 
-ggplot(data=shuklaVs,
+ggplot(data=shukla_out,
        aes(x=reorder(V,Estimate),
            y=Estimate)) +
   geom_point() +
@@ -87,11 +90,14 @@ ggplot(data=shuklaVs,
   theme(axis.text.x  = element_text(angle=90, hjust=1),
         axis.title.x = element_blank()) +
   scale_y_continuous(name="Shukla's stability variance with s.e.",
-                     limits = c(0,max(shuklaVs$Estimate+shuklaVs$StdErr)))
-  
+                     limits = c(0,max(shukla_out$Estimate+shukla_out$StdErr)))
+
+# Clean up
+rm(list=ls()[ls() %in% c("vc_names")])
+
 # FA-model Eberhart-Russel
 ##########################
-aman3 <- aman3[order(aman3$G,aman3$Env),] 
+dat3 <- dat3[order(dat3$G,dat3$Env),] 
 
 FAmod <- asreml(fixed   = adjmean ~ xj + tj,
                 random  = ~ G + L + Y + L:G + Y:G + fa(G):Env,
@@ -100,14 +106,14 @@ FAmod <- asreml(fixed   = adjmean ~ xj + tj,
                 control = asreml.control(pworkspace=8e10 ,
                                          workspace=8e8, 
                                          maxiter=100),
-                data    = aman3, ran.order = "user")
+                data    = dat3, ran.order = "user")
 
 # Format VC estimates
 FA_vc <- summary(FAmod)$varcomp[2:3]
 FA_vc$VC <- rownames(FA_vc)
 rownames(FA_vc) <- NULL
-FA_vc$isg  <- regexpr(":Env!G.",FA_vc$VC)
-FA_vc$isfa <- regexpr(".fa1",FA_vc$VC) 
+FA_vc$isg   <- regexpr(":Env!G.",FA_vc$VC)
+FA_vc$isfa  <- regexpr(".fa1",FA_vc$VC) 
 FA_vc$group <- NA
 FA_vc[which(FA_vc$isg>1) ,"group"] <- "sigma"
 FA_vc[which(FA_vc$isfa>1),"group"] <- "lambda"
@@ -119,9 +125,11 @@ FA_vc<-FA_vc[,c(3,1,2,6)]
 colnames(FA_vc) <- c("VC","Estimate","StdErr","group")
 
 # Transposed Output
-G_var <- subset(FA_vc, group=="sigma")[,-c(4)]
+G_var  <- subset(FA_vc, group=="sigma")[,-c(4)]
 colnames(G_var) <- c("VC","Var_Est","Var_SE")
-G_lam <- subset(FA_vc, group=="lambda")[,-c(4)]
+G_lam  <- subset(FA_vc, group=="lambda")[,-c(4)]
 colnames(G_lam) <- c("VC","Lambda_Est","Lambda_SE")
-FA_t <- merge(G_var, G_lam, by="VC")
+FA_out <- merge(G_var, G_lam, by="VC")
 
+# Clean up
+rm(list=ls()[ls() %in% c("G_lam","G_var")])

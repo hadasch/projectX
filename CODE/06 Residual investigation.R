8e6ea7a66070cf1cac4ce32f05362bf1a40a279b
Paul Schmidt <31928213+SchmidtPaul@users.noreply.github.com>
2018-02-02 10:13:09 +0100

    Five Models for GxE

--- CODE/06 Residual investigation.R
+++ CODE/06 Residual investigation.R
@@ -4,27 +4,50 @@ dataset <- "aman"
 #dataset <- "boro"
 #################
 
+dat3 <- read.delim(paste(dataset,"_means.txt",sep=""))
+dat3 <- dat3[order(dat3$G,dat3$Env),]
+dat3$Y <- as.factor(paste(dat3$Y))
+
+require(asreml)
+# Get residuals from standard model
+standard <- asreml(fixed   = adjmean ~ xj + tj,
+                random  = ~ G + L + Y + L:G + Y:G + at(G):Env,
+                ran.order = "user",
+                weights = w,
+                family  = asreml.gaussian(dispersion=1.0),
+                control = asreml.control(pworkspace=8e10 ,
+                                         workspace=8e8, 
+                                         maxiter=100),
+                data    = dat3)
+
+rstd <- dat3
+rstd$residstd <- standard$residuals
+
 rshuk <- read.delim(paste(dataset,"_resid_Shukla.txt",sep=""))
 rFA   <- read.delim(paste(dataset,"_resid_FA.txt",sep=""))
 
 resids <- merge(rshuk, rFA)
+resids <- merge(resids, rstd)
 
 plot(resids$tj, resids$residShukla)
 plot(resids$tj, resids$residFA)
+plot(resids$tj, resids$residstd)
 
 ####
 rshuk$model <- "Shukla"
 colnames(rshuk)[14] <- "resid"
 rFA$model <- "FA"
 colnames(rFA)[14]   <- "resid"
-resids2 <- rbind(rshuk, rFA)
+rstd$model <- "Standard"
+colnames(rstd)[14]   <- "resid"
+resids2 <- rbind(rshuk, rFA, rstd)
 
-
+require(ggplot2)
 resplot <- ggplot(data=resids2,
        aes(x=tj, y=resid, colour=model)) +
   geom_hline(yintercept=0, colour="grey") +
   geom_point(position = position_jitterdodge(
-              jitter.width = 0, dodge.width = 0.4)) +
+              jitter.width = 0.1, dodge.width = 0.8)) +
   ggtitle(dataset) +
   scale_x_continuous(name="Trial Year",
                      breaks=seq(min(resids2$tj),
